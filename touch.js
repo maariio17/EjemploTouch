8bb63f4458802ff4b515ec68038dea0c1b18785b
maariio17 <73340506+maariio17@users.noreply.github.com>
2020-10-30 12:42:18 +0100

    Ejemplo Touch

--- touch.js
+++ touch.js
@@ -1,37 +1,38 @@
-function startup() {
-    var el = document.getElementById("canvas");
-    el.addEventListener("touchstart", handleStart, false);
-    el.addEventListener("touchend", handleEnd, false);
-    el.addEventListener("touchcancel", handleCancel, false);
-    el.addEventListener("touchmove", handleMove, false);
-    //el.addEventListener("click",handleCancel);
-    if (window.DeviceOrientationEvent) {
-      window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-      
-    }
-  }
-  
-  document.addEventListener("DOMContentLoaded", startup);
+function inicio() {
+  var el = document.getElementById("canvas");
+  el.addEventListener("touchstart", handleStart, false);
+  el.addEventListener("touchend", handleEnd, false);
+//   el.addEventListener("touchcancel", handleCancel, false);
+  el.addEventListener("touchmove", handleMove, false);
+  el.addEventListener("click", click);
 
-  function handleStart(evt) {
-    evt.preventDefault();
-    document.getElementById("log").innerHTML = "Touchstart " + evt.touches[0].pageX + " "+evt.touches[0].pageY;
+  if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation',orientacion);
   }
-  function handleEnd(evt) {
-    evt.preventDefault();
-    document.getElementById("log").innerHTML = "Touchend " + evt.touches[0].pageX + " "+evt.touches[0].pageY;
-  }
-  function handleMove(evt) {
-    evt.preventDefault();
-    document.getElementById("log").innerHTML = "Touchmove " + evt.touches[0].pageX + " "+evt.touches[0].pageY;
-  }
+}
 
+window.onload = inicio;
 
-  function handleCancel(evt) {
-    evt.preventDefault();
-    log.innerHTML = "touchcancel.";
-  }
-  function deviceOrientationHandler(evt) {
-    evt.preventDefault();
-    log.innerHTML = evt.alpha;
-  }
+function handleStart(evt) {
+  evt.preventDefault();
+  document.getElementById("log").innerHTML = "Touchstart " + evt.touches[0].pageX + " "+evt.touches[0].pageY;
+}
+function handleEnd(evt) {
+  evt.preventDefault();
+  document.getElementById("log").innerHTML = "Touchend " + evt.touches[0].pageX + " "+evt.touches[0].pageY;
+}
+function handleMove(evt) {
+  evt.preventDefault();
+  document.getElementById("log").innerHTML = "Touchmove " + evt.touches[0].pageX + " "+evt.touches[0].pageY;
+}
+
+function click()
+{
+    alert("click");
+}
+
+function orientacion(e)
+{
+  document.getElementById("log").innerHTML = e.alpha;
+}
+
